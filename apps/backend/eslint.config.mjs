fba16d58307596e3f113a2c40bd5842e6e31bd6a
a-s-t-e-y-a <krishnoit100@gmail.com>
2025-08-08 13:38:14 +0530

    🚨 Add eslintrc and prettier

---
+++ apps/backend/eslint.config.mjs
@@ -0,0 +1,26 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+  {
+    ignores: ['node_modules/**', 'dist/**', '.wrangler/**'],
+  },
+];
